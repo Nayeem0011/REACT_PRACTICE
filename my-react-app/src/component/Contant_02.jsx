bc9dbece85fa51d8db89252766089086eb180b1c
Sk Nayeem <sknayeemislam384@.gmail.com>
2025-05-21 22:52:09 +0600

    PRECTICE_02

--- my-react-app/src/component/Contant_02.jsx
+++ my-react-app/src/component/Contant_02.jsx
@@ -6,14 +6,28 @@ const dataName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
+function Card() {
+  return <div className="card"> 
+           <h3 className="cardTitle">Call Family</h3>
+           <p className="cardDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, perferendis.</p>
+           <p className="cardFooter">{dataName + " / " + monthName + " / " + currentYear}</p>
+        </div>
+}
 
 function contant_02() {
   return (
     <>
-      <h1>Todo App</h1>
-      <h3 className="names">Call Family</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, perferendis.</p>
-      <p>{dataName + " / " + monthName + " / " + currentYear}</p>
+      <h1 className="names">Todo App</h1>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      {/* <div className="card"> 
+        <h3 className="cardTitle">Call Family</h3>
+        <p className="cardDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, perferendis.</p>
+        <p className="cardFooter">{dataName + " / " + monthName + " / " + currentYear}</p>
+      </div> */}
     </>
   )
 }
